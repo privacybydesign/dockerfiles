275c2789cd3a2b3efb019b506ea2995771860beb
Confiks <confiks@scriptbase.org>
2019-06-21 08:42:25 +0200

    chore: Update debian to stretch-20190610

--- debian-downloadrootfs.sh
+++ debian-downloadrootfs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# https://github.com/debuerreotype/docker-debian-artifacts/blob/a5e61a4c40a4b366d614715c51f883e0b153afb5/stretch/Dockerfile
-rootfs_url="https://github.com/debuerreotype/docker-debian-artifacts/raw/3e751c2c2f60037e9231ed94bbd1f95347af2c87/stretch/rootfs.tar.xz"
-rootfs_sha256="bce1c89311f017322830178228e1f7e074debca10255552a98daff924f457033"
+# https://github.com/debuerreotype/docker-debian-artifacts/blob/5cf98e568d562c62b507ba2b3fbfa1971b0c41e2/stable/Dockerfile
+rootfs_url="https://github.com/debuerreotype/docker-debian-artifacts/raw/5cf98e568d562c62b507ba2b3fbfa1971b0c41e2/stable/rootfs.tar.xz"
+rootfs_sha256="fe997e7cbdd9080809b2883b2c763ae6df384422148e1cbfd47067f043341c91"
 
 rootfs_path="${BASH_SOURCE%/*}/debian-rootfs.tar.xz"
 if [ ! -f "$rootfs_path" ]; then
