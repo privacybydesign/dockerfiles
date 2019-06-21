5c13cc6c706c2cba1edce0c3b6299b58b5e54b57
Confiks <confiks@scriptbase.org>
2019-06-21 10:11:50 +0200

    chore: Typo

--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Adapted from: https://github.com/docker-library/golang/blob/8f2f8502c173fbbcf33fcde5566ca53ad1070ae8/1.12/stretch/Dockerfile
 apt-get update
 apt-get install -y --no-install-recommends \
   ruby \
