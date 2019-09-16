2206aff18ba9afe4f053e0a9501a5d81784d4399
Confiks <confiks@scriptbase.org>
2019-09-16 18:56:48 +0200

    chore: Upgrade irma to v0.3.1

--- irma.sh
+++ irma.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget -q -O /usr/local/bin/irma https://github.com/privacybydesign/irmago/releases/download/v0.2.0/irma-master-linux-amd64
-echo "bbe5f9d1fdd1e2eeac04b86417c65abfbd4e84237fb76176b921835d4d03f9d5 /usr/local/bin/irma" | sha256sum -c
+wget -q -O /usr/local/bin/irma https://github.com/privacybydesign/irmago/releases/download/v0.3.1/irma-master-linux-amd64
+echo "f74bbafef1b37b954f233adb526c67cf3005b2fa9585cd0f8246054a85a5e7ba /usr/local/bin/irma" | sha256sum -c
 
 chmod +x /usr/local/bin/irma
