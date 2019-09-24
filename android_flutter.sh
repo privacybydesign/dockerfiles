520fd813282f9846ab091af4b85d1a4cb75b2020
Geert-Johan Riemer <geertjohan@geertjohan.net>
2019-09-24 13:15:36 +0200

    Upgrade to flutter 1.9.1+hotfix.2

--- android_flutter.sh
+++ android_flutter.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-FLUTTER_VERSION=1.7.8+hotfix.4-stable
-FLUTTER_CHECKSUM=c7d010c8037e64d2d3e2957f83bd8bee604ff6014230b09b8c043ef039538bd7
+FLUTTER_VERSION=1.9.1+hotfix.2
+FLUTTER_CHECKSUM=f82875a865c8dbebd10b7a69ffc4cb19d9c916054f3bbcda5a66395f30477d91
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
   lib32stdc++6
 rm -rf /var/lib/apt/lists/*
 
-wget -q -O flutter.tar.xz https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v${FLUTTER_VERSION}.tar.xz
+wget -q -O flutter.tar.xz https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v${FLUTTER_VERSION}-stable.tar.xz
 echo "${FLUTTER_CHECKSUM} *flutter.tar.xz" | sha256sum -c
 
 tar xf flutter.tar.xz
