17534beba8eace83b86ff9266ec4ae2d35967572
David Venhoek <david@venhoek.nl>
2019-11-08 09:26:35 +0100

    Added jinja2 to python3.

--- python3.sh
+++ python3.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  python3
+  python3 \
+  python3-jinja2
 rm -rf /var/lib/apt/lists/*
