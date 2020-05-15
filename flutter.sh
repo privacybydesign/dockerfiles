3cacef0db5075def0699891976a8d99b3387526c
Confiks <confiks@scriptbase.org>
2020-05-15 19:53:05 +0200

    chore: Bump flutter version by switchting to stable channel

--- flutter.sh
+++ flutter.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-FLUTTER_VERSION=1.17.0-dev.3.1
-FLUTTER_CHANNEL=beta
-FLUTTER_CHECKSUM=16d996182bd4d182cf8d7e3f433a0ae9d0cd615e4fef6a11efa9281800538b13
+FLUTTER_VERSION="1.17.1"
+FLUTTER_CHANNEL="stable"
+FLUTTER_CHECKSUM="ae40bc83504c147229b34755892e8df36678e312f66117904deaff2c8faf4a21"
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
