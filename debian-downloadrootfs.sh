0da36728e25127bdd131b2e27384f9bbc0f9356d
Confiks <confiks@scriptbase.org>
2020-05-15 19:53:06 +0200

    chore: Bump debian version

--- debian-downloadrootfs.sh
+++ debian-downloadrootfs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# https://github.com/debuerreotype/docker-debian-artifacts/blob/d6eeda93542f8e2a7d5f6e500b58fc4f12d055ce/buster/Dockerfile
-rootfs_url="https://github.com/debuerreotype/docker-debian-artifacts/raw/d6eeda93542f8e2a7d5f6e500b58fc4f12d055ce/buster/rootfs.tar.xz"
-rootfs_sha256="ede91c08bebcd7473af33fce173fba4218e460429909e791ab10be459e5079e4"
+# https://github.com/debuerreotype/docker-debian-artifacts/blob/18cb4d0418be1c80fb19141b69ac2e0600b2d601/buster/Dockerfile
+rootfs_url="https://github.com/debuerreotype/docker-debian-artifacts/raw/18cb4d0418be1c80fb19141b69ac2e0600b2d601/buster/rootfs.tar.xz"
+rootfs_sha256="e7d3a00b8dbf97e6dcb22b00ca9248cc2bf59d1a2107001a3ebf7cfb99542a1a"
 
 rootfs_path="${BASH_SOURCE%/*}/debian-rootfs.tar.xz"
 if [ ! -f "$rootfs_path" ]; then
