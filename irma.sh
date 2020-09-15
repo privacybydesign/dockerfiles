dd492c0945cf9a82007bb52eb81169ef96d0bd69
David Venhoek <david@venhoek.nl>
2020-09-15 09:57:39 +0200

    Update irma.sh

--- irma.sh
+++ irma.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget -q -O /usr/local/bin/irma https://github.com/privacybydesign/irmago/releases/download/v0.4.1/irma-master-linux-amd64
+wget -q -O /usr/local/bin/irma https://github.com/privacybydesign/irmago/releases/download/internal-requestors/irma-master-linux-amd64
 echo "34c735c289e4f37d7e5fe1893578f7cdef606ed4f292245672f96516d4f513fb /usr/local/bin/irma" | sha256sum -c
 
 chmod +x /usr/local/bin/irma
