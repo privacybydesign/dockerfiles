5a6e7eeda9acfa3f7d795e1745a5156f34baa324
David Venhoek <david@venhoek.nl>
2020-09-15 13:36:06 +0200

    Update irma binary checksum

--- irma.sh
+++ irma.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 wget -q -O /usr/local/bin/irma https://github.com/privacybydesign/irmago/releases/download/internal-requestors/irma-master-linux-amd64
-echo "34c735c289e4f37d7e5fe1893578f7cdef606ed4f292245672f96516d4f513fb /usr/local/bin/irma" | sha256sum -c
+echo "b5648edadc26bd5cc4ab75a86fa5db9289c59dae263336fd8304a354deb620d0 /usr/local/bin/irma" | sha256sum -c
 
 chmod +x /usr/local/bin/irma
