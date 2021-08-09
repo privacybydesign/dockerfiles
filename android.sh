fa71740b7aae1b53faa5051f1ffa4c47f64a0293
Sietse Ringers <mail@sietseringers.net>
2021-08-09 11:08:32 +0200

    chore: bump android to API level 30 and include android cmdline-tools

--- android.sh
+++ android.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wget -q -O sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
-echo "f10f9d5bca53cc27e2d210be2cbc7c0f1ee906ad9b868748d74d62e10f2c8275 sdk.zip" | sha256sum -c
+wget -q -O sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip
+echo "124f2d5115eee365df6cf3228ffbca6fc3911d16f8025bebd5b1c6e2fcfa7faf sdk.zip" | sha256sum -c
 
 unzip -q sdk.zip -d "$ANDROID_HOME"
 rm sdk.zip
@@ -16,6 +16,6 @@ sdkmanager --sdk_root="$ANDROID_HOME" \
   "platform-tools" \
   "ndk;21.4.7075529" \
   "cmake;3.10.2.4988404" \
-  "platforms;android-28" \
-  "build-tools;28.0.3" \
-  >/dev/null
+  "platforms;android-30" \
+  "build-tools;30.0.3" \
+  "cmdline-tools;3.0"
