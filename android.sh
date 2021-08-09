13f3ab79f10b9a50a096ddbead0ead29b45b1c80
Sietse Ringers <mail@sietseringers.net>
2021-08-09 15:20:24 +0200

    chore: install all android SDKs required by flutter

--- android.sh
+++ android.sh
@@ -12,10 +12,15 @@ set +o pipefail
 yes | sdkmanager --sdk_root="$ANDROID_HOME" --licenses
 set -o pipefail
 
+# Flutter 2 needs Android SDK platform 28, 29 and 30 and build-tools 28 and 30. We pack them into
+# the Docker image to prevent that Flutter downloads them on every app build.
 sdkmanager --sdk_root="$ANDROID_HOME" \
   "platform-tools" \
   "ndk;21.4.7075529" \
   "cmake;3.10.2.4988404" \
+  "platforms;android-28" \
+  "platforms;android-29" \
   "platforms;android-30" \
+  "build-tools;28.0.3" \
   "build-tools;30.0.3" \
   "cmdline-tools;3.0"
