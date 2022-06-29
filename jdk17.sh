d124293b9781d5b5f09edf432fc4c33103c9e8b3
Sander Hollaar <sander.hollaar@sidn.nl>
2022-06-29 15:28:27 +0200

    feat: upgrade gradle from 4.10 to 7.5

--- jdk17.sh
+++ jdk17.sh
@@ -2,8 +2,19 @@
 set -euxo pipefail
 
 apt-get update
+apt-get -y dist-upgrade
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  openjdk-17-jdk-headless \
-  gradle
+  curl openjdk-17-jdk-headless unzip
 apt-get clean
 rm -rf /var/lib/apt/lists/*
+
+curl -sLo/tmp/gradle.zip https://services.gradle.org/distributions/gradle-7.5-rc-2-bin.zip
+unzip -d /opt/ /tmp/gradle.zip
+
+cat > /etc/profile.d/gradle.sh <<EOD
+if ! echo ${PATH} | grep -q gradle
+then
+   PATH=${PATH}:/opt/gradle-7.5-rc-2/bin
+fi
+EOD
+chmod 755 /etc/profile.d/gradle.sh
